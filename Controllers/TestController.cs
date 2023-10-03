da3303e631b9c3867729868ef9370443f18f1490
ksmeduri <58795583+ksmeduri@users.noreply.github.com>
2023-10-03 16:50:53 -0500

    Added test controller
    Added test controller

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -6,6 +6,13 @@ namespace WebApplication1.Controllers
     [Route("[controller]")]
     public class TestController : ControllerBase
     {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
         [HttpGet(Name = "GetTest")]
         public IEnumerable<WeatherForecast> Get() 
         {
--- Program.cs
+++ Program.cs
@@ -2,9 +2,26 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication1;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
 
 //app.MapGet("/todoitems", async (TodoDb db) =>
 //    await db.Todos.ToListAsync());
@@ -52,4 +69,4 @@ var app = builder.Build();
 //    return Results.NotFound();
 //});
 
-app.Run();
+//app.Run();
