5e7b4c0635a1ebd6469d0bc5a1cfaeaaadc250f5
ksmeduri <58795583+ksmeduri@users.noreply.github.com>
2023-10-03 16:37:22 -0500

    Initial commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(Name = "GetTest")]
+        public IEnumerable<WeatherForecast> Get() 
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = 32,
+                Summary = ""
+            }).ToArray();
+        }
+    }
+}
